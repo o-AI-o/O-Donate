3bae0f6dd4cbcf55e62b62aaefe2a2444c284202
Kittisak Sangkarak <MinaseKoito09@gmail.com>
2020-06-11 16:27:52 +0700

    11/06

---
+++ routes/fundraiser.js
@@ -0,0 +1,29 @@
+const   express         = require('express'),
+        passport        = require('passport');
+
+const   db_user         = require('../models/db_user'),
+        middleware      = require('../middleware');
+
+const   router          = express.Router();
+
+router.get("/", function(req, res){
+    res.render("fundraiser/category");
+});
+
+router.get("/:id", function(req, res){
+
+});
+
+router.get("/:id/donate", function(req, res){
+
+});
+
+router.get("/category", function(req, res){
+
+});
+
+// router.get("/category", function(req, res){
+//     res.render("fundraiser/category");
+// });
+
+module.exports = router;
