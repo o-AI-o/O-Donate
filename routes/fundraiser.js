4013dc9b7cd6ca176ff66e21de3e209f59498238
Kittisak Sangkarak <MinaseKoito09@gmail.com>
2020-06-20 16:47:12 +0700

    Fundrai AddPic

--- routes/fundraiser.js
+++ routes/fundraiser.js
@@ -1,11 +1,29 @@
 const   express         = require('express'),
-        passport        = require('passport');
+        multer          = require('multer'),
+        path            = require('path');
 
-const   db_user         = require('../models/db_user');
 const   db_fundraiser   = require('../models/db_fundraiser'),
         middleware      = require('../middleware');
 
 const   router          = express.Router();
+
+const   storage         = multer.diskStorage({
+    destination: './public/uploads/fundraisers',
+    filename: function(req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+
+const   imageFilter     = function(req, file, cb){
+    var ext = path.extname(file.originalname);
+    if(ext == '.png' || ext !== '.jpg' || ext !== '.jpeg'){
+        cb(null, true);
+    } else {
+        return cb(new Error('ERROR'), false);
+    }
+}
+
+const   upload          = multer({storage: storage, fileFilter: imageFilter});
 
 router.get("/", function(req, res){
     res.render("fundraiser/category");
@@ -23,12 +41,12 @@ router.get("/add", function(req, res){
     res.render("fundraiser/addFundraiser");
 });
 
-router.post("/add", function(req, res){
+router.post("/add", upload.single('Image'), function(req, res){
     const fundraiReg = new db_fundraiser({
         fund_name: req.body.fname,
         fund_title: req.body.title,
         fund_description: req.body.description,
-        fund_image: "https://s3-ap-northeast-1.amazonaws.com/anime.bang-dream.com/pico/wp-content/uploads/2020/06/PICO06_03_HP.jpg",
+        fund_image: req.file.filename,
         fund_moneytraget: req.body.target,
         fund_moneynow: 0,
         fundHistory: [],
